872df2f8bf10be8757308e78afe38d6701e88860
beatdjam <beat3526@gmail.com>
2020-11-29 17:44:43 +0900

    initial commit (with ViewBinding config)

---
+++ app/src/main/java/com/beatdjam/androidfundamentals/Unit1Activity.kt
@@ -0,0 +1,11 @@
+package com.beatdjam.androidfundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Unit1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unit1)
+    }
+}
